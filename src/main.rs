5a36722e4a522cdf56a9257fb7a2f988171b1ccc
moontai0724 <moontai0724@gmail.com>
2025-03-25 13:10:22 +0800

    feat: hello world as index

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use serde_json::{ json, Value };
+
+#[get("/")]
+fn index() -> Json<Value> {
+    Json(json!({"hello": "world"}))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
