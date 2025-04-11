0453292d244ec8bce564fbd0a8fadad1b717b854
moontai0724 <moontai0724@gmail.com>
2025-04-11 13:18:37 +0800

    feat(database): setup connection

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@ extern crate rocket;
 mod middlewares;
 mod persistances;
 
-use middlewares::keycloak::Token;
-use rocket::{fairing::AdHoc, http::Status, serde::json::Json};
+use database::{
+    entity::*,
+    migration::{self, MigratorTrait},
+};
+use middlewares::{database::DatabaseClient, keycloak::Token};
+use rocket::{
+    Build, Rocket,
+    fairing::{self, AdHoc},
+    http::Status,
+    serde::json::Json,
+};
+use sea_orm::EntityTrait;
+use sea_orm_rocket::{Connection, Database};
 use serde_json::{Value, json};
 
 #[get("/")]
-fn index(token: Token) -> Json<Value> {
-    Json(json!({"hello": "world", "token": token}))
+async fn index(token: Token, conn: Connection<'_, DatabaseClient>) -> Json<Value> {
+    let users = users::Entity::find().all(conn.into_inner()).await.unwrap();
+
+    Json(json!({"hello": "world", "users": users, "token": token}))
 }
 
 #[get("/heartbeat")]
@@ -18,9 +31,17 @@ fn health_check() -> Status {
     Status::NoContent
 }
 
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+    let conn = &DatabaseClient::fetch(&rocket).unwrap().conn;
+    let _ = migration::Migrator::up(conn, None).await;
+    Ok(rocket)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(DatabaseClient::init())
+        .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
         .mount("/", routes![index, health_check])
         .attach(AdHoc::config::<persistances::config::AppConfig>())
 }
--- src/persistances/config.rs
+++ src/persistances/config.rs
@@ -6,3 +6,22 @@ pub struct AppConfig {
     pub sso_audience: Vec<String>,
     pub sso_issuer: Vec<String>,
 }
+
+#[derive(Debug, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct DatabaseConfig {
+    pub db_username: String,
+    pub db_password: String,
+    pub db_host: String,
+    pub db_port: u16,
+    pub db_database: String,
+}
+
+impl DatabaseConfig {
+    pub fn url(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}/{}",
+            self.db_username, self.db_password, self.db_host, self.db_port, self.db_database
+        )
+    }
+}
