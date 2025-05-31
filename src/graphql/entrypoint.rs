30ef6d5ea1d8aeb3a6f7e8466a4e14baeb914fb9
moontai0724 <moontai0724@gmail.com>
2025-05-31 17:34:08 +0800

    feat(auth): add authorization to graphql endpoint

--- src/graphql/entrypoint.rs
+++ src/graphql/entrypoint.rs
@@ -2,16 +2,24 @@ use async_graphql::{EmptyMutation, EmptySubscription, Schema};
 use async_graphql_rocket::*;
 use rocket::State;
 
-use crate::QueryRoot;
+use crate::{QueryRoot, middlewares::user::User};
 
 type SchemaType = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+pub struct QueryData {
+    pub user: User,
+}
 
 #[rocket::post("/graphql", data = "<request>", format = "application/json")]
 pub async fn graphql_request(
     schema: &State<SchemaType>,
     request: GraphQLRequest,
+    user: User,
 ) -> GraphQLResponse {
-    request.execute(schema.inner()).await
+    request
+        .data(QueryData { user })
+        .execute(schema.inner())
+        .await
 }
 
 pub fn build_schema() -> SchemaType {
--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -1,10 +1,24 @@
-use async_graphql::Object;
+use async_graphql::{Context, Object};
+use database::entity::{prelude::*, *};
+use sea_orm::{DbErr, EntityTrait};
+
+use crate::persistances::db;
+
+use super::entrypoint::QueryData;
 
 pub(crate) struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn hello(&self) -> String {
-        "Hello GraphQL".to_owned()
+    async fn me(&self, ctx: &Context<'_>) -> Result<users::Model, async_graphql::Error> {
+        Ok(ctx.data::<QueryData>().unwrap().user.from_db.clone())
+    }
+
+    async fn users(&self) -> Result<Vec<users::Model>, DbErr> {
+        Users::find().all(&**db::get()).await
+    }
+
+    async fn user(&self, id: u64) -> Result<Option<users::Model>, DbErr> {
+        Users::find_by_id(id).one(&**db::get()).await
     }
 }
