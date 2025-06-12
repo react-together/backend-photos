fa471e492e0edf9dd4f846b4c4a0b35ed9358aa9
moontai0724 <moontai0724@gmail.com>
2025-06-12 21:19:12 +0800

    feat: add options route

--- src/graphql/entrypoint.rs
+++ src/graphql/entrypoint.rs
@@ -1,8 +1,15 @@
 use crate::{middlewares::user::User, persistances::db};
 use async_graphql_rocket::*;
+use rocket::http::Status;
 
 pub struct QueryData {
     pub user: User,
+}
+
+/// Manually mount an OPTIONS route for your own handling
+#[options("/graphql")]
+pub fn graphql_options() -> Status {
+    Status::Ok
 }
 
 #[rocket::post("/graphql", data = "<request>", format = "application/json")]
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,14 @@ async fn rocket() -> Rocket<Build> {
     let _ = Migrator::up(&**db::get(), None).await;
 
     let instance = rocket::build()
-        .mount("/", routes![health_check, entrypoint::graphql_request])
+        .mount(
+            "/",
+            routes![
+                health_check,
+                entrypoint::graphql_options,
+                entrypoint::graphql_request
+            ],
+        )
         .attach(AdHoc::config::<persistances::config::AppConfig>())
         .attach(middlewares::cors::CORS);
 
